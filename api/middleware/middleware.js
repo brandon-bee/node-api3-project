4ff8cad0283d2f1eeec8c609154f3444fb4f71c1
Brandon B <64279000+brandon-bee@users.noreply.github.com>
2022-02-09 13:32:05 -0800

    middleware done and connected to router

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,59 @@
+const { getById } = require('../users/users-model');
+
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  const timestamp = new Date().toLocaleString();
+  const { method, url } = req;
+  console.log(`[${timestamp}] ${method} to ${url}`);
+  next();
 }
 
 function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+  const { id } = req.params;
+  getById(id)
+  .then(user => {
+    if (user) {
+      req.user = user;
+      next();
+    } else {
+      next({
+        status: 404,
+        message: 'user not found'
+      });
+    }
+  })
+  .catch(next);
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+  let { name } = req.body;
+  if (name && name.trim()) {
+    name = name.trim();
+    next();
+  } else {
+    next({
+      status: 400,
+      message: 'missing required name field'
+    });
+  }
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+  let { text } = req.body;
+  if (text) {
+    text = text.trim();
+    next();
+  } else {
+    next({
+      status: 400,
+      message: 'missing required text field'
+    });
+  }
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost
+}
--- api/server.js
+++ api/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const usersRouter = require('./users/users-router');
 
 const server = express();
 
 // remember express by default cannot parse JSON in request bodies
 server.use(express.json());
 // global middlewares and the user's router need to be connected here
+server.use(cors());
+server.use(morgan('tiny'));
+server.use('/api/users', usersRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
